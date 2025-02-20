11eaaa0737be38bc0902739662e79a4527e82e0f
Basel_Holaiel <143265738+basel402@users.noreply.github.com>
2025-02-20 20:45:15 +0200

    Create insertion sort.cpp

---
+++ insertion sort.cpp
@@ -0,0 +1,19 @@
+vector<int> insertion_sort() {
+    int n;cin >> n;
+    vector<int>arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    for (int i = 1; i < n ; ++i) {
+        int tmp = arr[i];
+        int j = i;
+        for ( ; j > 0 && tmp<arr[j-1]; --j) {
+            arr[j] = arr[j-1];
+        }
+        arr[j] = tmp;
+
+    }
+    for(auto it:arr)cout << it << ' ';
+    return arr;
+}
