6bb46afa852614d8c1227c5e256f930234148e7f
Basel_Holaiel <143265738+basel402@users.noreply.github.com>
2025-02-25 14:55:40 +0200

    Update selection sort.cpp

--- selection sort.cpp
+++ selection sort.cpp
@@ -1,5 +1,6 @@
 void selection_sort(){
-    // selection sort is better in movement but worse in comparison than insertion both stable and implace
+    // selection sort is better in movement but worse in comparison than insertion
+    // unstable(equal elements may be exchanged and not sorted according to who came first in the array) and in_place(didnt use extra memory just the mn var)
     int n;cin >> n;
     vector<ll>v(n);
     for (int i = 0; i < n; ++i) {
