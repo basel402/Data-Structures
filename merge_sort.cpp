4d86988c25e2844528070c944037111e8137d571
Basel_Holaiel <143265738+basel402@users.noreply.github.com>
2025-03-10 22:48:48 +0200

    Create merge_sort.cpp

---
+++ merge_sort.cpp
@@ -0,0 +1,41 @@
+void merge(vector<ll>&v, ll l , ll r , ll mid){
+    ll arr1[mid-l+1],arr2[r-mid];
+    for (ll i = 0; i < mid-l+1 ; ++i) {
+        arr1[i] = v[l+i];
+    }
+    for (ll i = 0; i < r-mid ; ++i) {
+        arr2[i] = v[mid+1+i];
+    }
+    ll i = 0 , j = 0 , k = l;
+    while(i<mid-l+1 && j<r-mid){
+        if(arr1[i]<arr2[j]){
+            v[k] = arr1[i];
+            i++;
+        }
+        else{
+            v[k] = arr2[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<mid-l+1){
+        v[k] = arr1[i];
+        i++;
+        k++;
+    }
+    while(j<r-mid){
+        v[k] = arr2[j];
+        j++;
+        k++;
+    }
+}
+
+void merge_sort(vector<ll>&v , ll l , ll r){
+    if(l >= r)return;
+
+    ll mid = (l+r)/2;
+
+    merge_sort(v,l,mid);
+    merge_sort(v,mid+1,r);
+    merge(v,l,r,mid);
+}
