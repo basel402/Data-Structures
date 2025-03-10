dfdea1e65d18052845986a209b5cfe4f746d5e63
Basel_Holaiel <143265738+basel402@users.noreply.github.com>
2025-03-10 23:25:59 +0200

    Create count_sort.cpp

---
+++ count_sort.cpp
@@ -0,0 +1,23 @@
+void count_sort(int arr[], int n)
+{
+  
+    int k = *max_element(arr, arr + n);
+    int count[k + 1] = { 0 };
+
+    for (int i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+
+    for (int i = 1; i <= k; i++) {
+        count[i] = count[i] + count[i - 1];
+    }
+  
+    int ans[n];
+    for (int i = n - 1; i >= 0; i--) {
+        ans[--count[arr[i]]] = arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = ans[i];
+    }
+}
