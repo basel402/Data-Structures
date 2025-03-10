101c4ac3bbecfc4c143df47f710dd6e1f0b41a1a
Basel_Holaiel <143265738+basel402@users.noreply.github.com>
2025-03-10 23:15:00 +0200

    Create quick_sort.cpp

---
+++ quick_sort.cpp
@@ -0,0 +1,26 @@
+int partition(vector<int> &vec, int low, int high) {
+
+    
+    int pivot = vec[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (vec[j] <= pivot) {
+            i++;
+            swap(vec[i], vec[j]);
+        }
+    }
+
+    swap(vec[i + 1], vec[high]);
+
+    return (i + 1);
+}
+
+void quickSort(vector<int> &vec, int low, int high) {
+
+    if (low < high) {
+        int pi = partition(vec, low, high);
+        quickSort(vec, low, pi - 1);
+        quickSort(vec, pi + 1, high);
+    }
+}
