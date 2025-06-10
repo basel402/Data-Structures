1665e3123a8b068b7090879c4b9b64fbc0bdb596
Basel_Holaiel <143265738+basel402@users.noreply.github.com>
2025-06-10 23:57:43 +0300

    Update bubble sort.cpp

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -1,16 +1,10 @@
-void bubble_sort(){
-    ll n;cin >> n;
-    vector<ll>v(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> v[i];
-    }
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n-i && j<n-1; ++j) {
-            if(v[j]>v[j+1])swap(v[j],v[j+1]);
-        }
-    }
-    for (int i = 0; i < n; ++i) {
-        cout << v[i] << endl;
-    }
+void bubbleSort(T data[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+    
+        for (int j = n - 1; j > i; --j)
+    
+            if (data [j] < data [j-1])
+                swap (data [j], data [j - 1]);
+
 } // in_place and stable
 // all cases n^2
